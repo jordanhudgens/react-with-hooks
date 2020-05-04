8b9757815f378500e730a4a8b4dc2fab19ba51c3
jordanhudgens <jordan@devcamp.com>
2020-05-04 09:32:26 -0700

    A_05_analyzing_app_js_component

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,7 @@ export default class App extends Component {
     return (
       <div className="app">
         <h1>DevCamp React Starter</h1>
-        <h2>React + Redux</h2>
-
-        <div style={{ color: "red" }}>
-          Warning: this is the experimental template, pardon the bugs
-        </div>
+        <h2>React + React Router</h2>
       </div>
     );
   }
