bc5c88d4fc4b9a1e833a12927720e76b98189fcc
jordanhudgens <jordan@devcamp.com>
2020-05-05 12:23:19 -0700

    A_06_three_ways_to_implement_styles_in_react

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
+    const dynamicBackgroundColor = "black";
+
+    const styles = {
+      height: "100px",
+      backgroundColor: dynamicBackgroundColor,
+      color: "white",
+    };
+
     return (
       <div className="app">
         <h1>DevCamp React Starter</h1>
         <h2>React + React Router</h2>
+
+        <div style={styles}>Testing styles</div>
       </div>
     );
   }
