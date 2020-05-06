d70ab1aa4d371f85951e553629bdd56ab19146f5
jordanhudgens <jordan@devcamp.com>
2020-05-06 10:55:52 -0700

    A_08_introduction_to_organizing_react_components

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
-const NavBar = () => {
+import NavBar from "./shared/NavBar";
+
+const App = () => {
+  const dynamicBackgroundColor = "black";
+
+  const styles = {
+    height: "100px",
+    backgroundColor: dynamicBackgroundColor,
+    color: "white",
+  };
+
   return (
-    <div>
-      <div>Jordan Hudgens</div>
+    <div className="app">
+      <NavBar />
+      <h1>DevCamp React Starter</h1>
+      <h2>React + React Router</h2>
 
-      <div>Home</div>
-
-      <div>Portfolio</div>
+      <div style={styles}>Testing styles</div>
+      <NavBar />
     </div>
   );
 };
 
-export default class App extends Component {
-  render() {
-    const dynamicBackgroundColor = "black";
-
-    const styles = {
-      height: "100px",
-      backgroundColor: dynamicBackgroundColor,
-      color: "white",
-    };
-
-    return (
-      <div className="app">
-        <NavBar />
-        <h1>DevCamp React Starter</h1>
-        <h2>React + React Router</h2>
-
-        <div style={styles}>Testing styles</div>
-        <NavBar />
-      </div>
-    );
-  }
-}
+export default App;
---
+++ src/components/shared/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>Jordan Hudgens</div>
+
+      <div>Home</div>
+
+      <div>Portfolio</div>
+    </div>
+  );
+};
+
+export default NavBar;
