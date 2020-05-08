3aef9838fae7e20c78e352610b28a45a8f72fb12
jordanhudgens <jordan@devcamp.com>
2020-05-08 10:21:20 -0700

    A_12_static_images_react

--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+const logo = require("../../../static/assets/images/code-2.png");
+
 export default () => {
   return (
     <div>
+      <img src={logo} />
       <div>Jordan Hudgens</div>
 
       <div>Home</div>
