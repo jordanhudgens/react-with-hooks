f06f31badfb20705223e6d1ae04222016099f566
jordanhudgens <jordan@devcamp.com>
2020-05-20 09:30:59 -0700

    B_10_implementing_loading_state_in_react

--- src/components/portfolio/PortfolioItemsList.js
+++ src/components/portfolio/PortfolioItemsList.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import PortfolioItem from "./PortfolioItem";
+import Loader from "../shared/Loader";
 
 const API_URL =
   "https://reactportfolio.devcamp.space/portfolio/portfolio_items";
 
 export default () => {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getPortfolioItems();
@@ -16,12 +18,17 @@ export default () => {
   const getPortfolioItems = () => {
     axios.get(API_URL).then((response) => {
       setItems(response.data.portfolio_items);
+      setIsLoading(false);
     });
   };
 
   const itemsRenderer = () => {
     return items.map((item) => <PortfolioItem key={item.id} item={item} />);
   };
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return <div>{itemsRenderer()}</div>;
 };
