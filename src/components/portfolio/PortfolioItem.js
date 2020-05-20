e8b74c18f2812047c86acbcec6691b06ffb76853
jordanhudgens <jordan@devcamp.com>
2020-05-20 09:13:34 -0700

    B_09_how_to_view_and_render_json_content_from_api

--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+// <pre>{JSON.stringify(props.item, null, 2)}</pre>
+
 export default (props) => {
-  const { name, description } = props.item;
+  const { name, description, thumb_image_url, logo_url } = props.item;
   return (
     <div>
       <div>{name}</div>
       <div>{description}</div>
+      <img src={thumb_image_url} style={{ width: "100%" }} />
+      <img src={logo_url} style={{ width: "100%" }} />
     </div>
   );
 };
