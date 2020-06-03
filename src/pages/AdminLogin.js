711efdc243bfeb242770183ffa375e8c14b861e8
jordanhudgens <jordan@devcamp.com>
2020-06-03 09:49:27 -0700

    B_12_introduction_react_forms

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -2,17 +2,31 @@ import React, { useState } from "react";
 
 export default () => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("Email", email);
+    console.log("Password", password);
+  };
 
   return (
     <div>
       Admin login
-      <h2>Email: {email}</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           value={email}
           onChange={(evt) => setEmail(evt.target.value)}
           type="email"
         />
+
+        <input
+          value={password}
+          onChange={(evt) => setPassword(evt.target.value)}
+          type="password"
+        />
+
+        <button type="submit">Login</button>
       </form>
     </div>
   );
