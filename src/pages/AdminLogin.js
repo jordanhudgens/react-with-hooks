ea5ce219f592525be65b264bbbf0c003b8e1179f
jordanhudgens <jordan@devcamp.com>
2020-06-05 13:18:18 -0700

    B_13_working_with_jwt_tokens

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+const API_URL = "https://api.devcamp.space/client_token";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState(null);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log("Email", email);
-    console.log("Password", password);
+
+    const params = {
+      auth: {
+        email: email,
+        password: password,
+      },
+    };
+
+    axios
+      .post(API_URL, params)
+      .then((response) => {
+        if (response.data.jwt) {
+          console.log("JWT", response.data.jwt);
+        } else {
+          setErrorText("There was an error logging you in, please try again");
+        }
+      })
+      .catch((error) => {
+        console.log("Errors", error);
+        setErrorText("There was an error logging you in, please try again");
+      });
   };
 
   return (
     <div>
       Admin login
+      <div>{errorText}</div>
       <form onSubmit={handleSubmit}>
         <input
           value={email}
