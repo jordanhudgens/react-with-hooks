110ae5ebd6d4b8d7050e2e13c63c559172a6d511
jordanhudgens <jordan@devcamp.com>
2020-06-09 11:39:51 -0700

    B_16_building_check_login_function

--- src/providers/AdminProvider.js
+++ src/providers/AdminProvider.js
@@ -1,9 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import AdminContext from "../contexts/AdminContext";
 
+const API_URL = "https://api.devcamp.space/token_logged_in";
+
 export default (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("devcamp_space_secure_token");
+
+    if (token) {
+      checkLogin(token);
+    }
+  }, []);
+
+  const checkLogin = (token) => {
+    axios
+      .get(API_URL, { headers: { Authorization: `Bearer ${token}` } })
+      .then((response) => {
+        console.log("res", response.data);
+        if (response.data.client) {
+          setIsLoggedIn(true);
+        }
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
 
   const stateValue = {
     isLoggedIn,
