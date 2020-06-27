d7e8d5418821b8c24c4d202bbc1aa2f8e940e4d2
jordanhudgens <jordan@devcamp.com>
2020-06-26 17:37:36 -0700

    C_11_Retrieving_Single_Record_API_React

--- src/pages/PortfolioDetail.js
+++ src/pages/PortfolioDetail.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DashboardLayout from "../components/layouts/DashboardLayout";
+import Axios from "axios";
 
 export default (props) => {
-  console.log("Props for the portfolio detail", props);
+  useEffect(() => {
+    getPortfolioItem(props.match.params.id);
+  }, []);
+
+  const getPortfolioItem = (id) => {
+    const api_url = `https://reactportfolio.devcamp.space/portfolio/portfolio_items/${id}`;
+
+    Axios.get(api_url)
+      .then((response) => {
+        console.log("portfolio item", response.data);
+      })
+      .catch((error) => {
+        console.log("errors", error);
+      });
+  };
 
   return (
     <DashboardLayout>
-      <h1>Portfolio detail for: {props.match.params.id}</h1>
+      <h1>Portfolio detail</h1>
     </DashboardLayout>
   );
 };
