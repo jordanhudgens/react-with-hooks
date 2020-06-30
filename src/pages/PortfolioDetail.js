aec844cd37bbb1972ea2ecd5a0b2b2f0145cf942
jordanhudgens <jordan@devcamp.com>
2020-06-30 14:59:50 -0700

    C_12_Rendering_Data_Portfolio_Detail_Page

--- src/pages/PortfolioDetail.js
+++ src/pages/PortfolioDetail.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../components/layouts/DashboardLayout";
 import Axios from "axios";
 
 export default (props) => {
+  const [portfolioItem, setPortfolioItem] = useState(null);
+
   useEffect(() => {
     getPortfolioItem(props.match.params.id);
   }, []);
@@ -13,15 +15,36 @@ export default (props) => {
     Axios.get(api_url)
       .then((response) => {
         console.log("portfolio item", response.data);
+        setPortfolioItem(response.data.portfolio_item);
       })
       .catch((error) => {
         console.log("errors", error);
       });
   };
 
+  if (!portfolioItem) {
+    return <div>Loading...</div>;
+  }
+
+  const { banner_image_url, description, logo_url, name, url } = portfolioItem;
+
   return (
     <DashboardLayout>
-      <h1>Portfolio detail</h1>
+      <div className="portfolio-detail-wrapper">
+        <div>
+          <img src={logo_url} />
+        </div>
+
+        <div className="portfolio-detail-description">{description}</div>
+
+        <h1>{banner_image_url}</h1>
+
+        <div className="bottom-content-wrapper">
+          <a href={url} target="_blank">
+            Visit {name}
+          </a>
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
